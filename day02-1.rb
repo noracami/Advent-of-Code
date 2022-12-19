f440a1fb47a01a1890aa7e38f90a9b12d0a4a34c
noracami <kerke.2011@gmail.com>
2022-12-19 14:35:40 +0800

    day2 part2

--- day02-1.rb
+++ day02-1.rb
@@ -17,9 +17,27 @@ def score(opponent, yourself)
   shape[yourself.to_sym] + outcome[duel(opponent, yourself).to_sym]
 end
 
+def score2(opponent, yourself)
+  shape = {'A': 1, 'B': 2, 'C': 3}
+  result = {'X': 2, 'Y': 0, 'Z': 1}
+  outcome = {'X': 0, 'Y': 3, 'Z': 6}
+
+  shape_pt = shape[opponent.to_sym] + result[yourself.to_sym]
+  shape_pt = shape_pt > 3 ? shape_pt % 3 : shape_pt
+  
+  shape_pt + outcome[yourself.to_sym]
+end
+
 File.open('day02-1-input.txt', 'r') { |f|
   res = f.reduce(0) { |sum, line|
     sum + score(line[0], line[2])
   }
   p res
+}
+
+File.open('day02-1-input.txt', 'r') { |f|
+  res2 = f.reduce(0) { |sum, line|
+    sum + score2(line[0], line[2])
+  }
+  p res2
 }
