92fb172fedc367f80370297909ac39c7ff7f8572
noracami <kerke.2011@gmail.com>
2022-12-19 16:57:02 +0800

    day03 part 2

--- day03-2.rb
+++ day03-2.rb
@@ -1,18 +1,26 @@
 File.open('day03-input.txt', 'r') { |f|
-  priority = -> (char) {
+  def priority (char)
     if char.ord >= 'a'.ord
       char.ord - 'a'.ord + 1
     elsif char.ord <= 'Z'.ord
       char.ord - 'A'.ord + 1 + 26
     end
-  }
-  
-  # p f.each_slice(3).first 2
-  
-  res = f.each_slice(3).first(2).reduce(0) { |sum, rucksack|
-    p sum
-    p rucksack
-    # sum += [rucksack[0, len], rucksack[len, len]].map(&:chars).inject { |a, b| a - (a - b) }.map(&priority).last
-  }
+  end
 
+  def intersection (a, b)
+    b.reduce("") { |memo, char|
+      if a.include? char
+        memo + char
+      else
+        memo
+      end
+    }.chars.uniq
+  end
+  
+  res = f.each_slice(3).reduce(0) { |sum, rucksack|
+    a, b, c = rucksack.map(&:chomp).map(&:chars)
+    sum += priority intersection(*[intersection(*[a, b]), c]).last
+  }
+  
+  p res
 }
