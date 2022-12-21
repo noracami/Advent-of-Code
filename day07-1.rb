b7d99a5504984b70073d8f720026f55cd1fb5f24
noracami <kerke.2011@gmail.com>
2022-12-21 19:47:02 +0800

    day07 part1

--- day07-1.rb
+++ day07-1.rb
@@ -1,33 +1,47 @@
-class Folder
-  attr_accessor :name, :size
+
+
+# class Folder
+#   attr_accessor :name, :size
   
-  def initialize(name="")
-    @name = name
-    @size = 0
-    @sub_folder = []
-    @files = []
-  end
+#   def initialize(name="", parent_folder=nil)
+#     @name = name
+#     @size = 0
+#     @parent_folder = parent_folder
+#     @files = []
+#   end
 
-  def << item
-    _ = item
-    name, size = _[:name], _[:size]
-    if size
-      @files << {name:, size:}
-      @size += size.to_i
-    else
-      @sub_folder << Folder.new(name)
-    end
-  end
+#   def << item
+#     _ = item
+#     name, size = _[:name], _[:size]
+#     is_file? = !size.nil?
+    
+#     if is_file?
+#       @files << {name:, size:}
+#       @size += size.to_i
+#     else
+#       @sub_folder << Folder.new(name)
+#     end
+#   end
 
-  def to_s
-    if @sub_folder.empty?
-      "name: #{@name}, size: #{@size}"
-    else
-      sub_folder = @sub_folder.map(&:to_s)
-      "name: #{@name}, size: #{@size}, sub_folder: #{sub_folder}"
-    end
-  end
-end
+#   def to_s
+#     if @sub_folder.empty?
+#       "name: #{@name}, size: #{@size}"
+#     else
+#       sub_folder = @sub_folder.map(&:to_s)
+#       "name: #{@name}, size: #{@size}, sub_folder: #{sub_folder}"
+#     end
+#   end
+# end
+
+# class Directory
+#   attr_accessor
+
+#   @current_directory = "root"
+#   @previous_directory = []
+#   @content = {root: Folder.new("root")}
+
+  
+# end
 
 def parse(str)
   if str.class.name == "Array"
@@ -54,13 +68,15 @@ end
 File.open('day07-input.txt', 'r') { |f|
   f.readline
 
-  tree = Folder.new("root")
-  current_directory = 0
-
-  res = f.first 20
+  # res = f.first 11
+  res = f
 
   res = res.chunk_while { |i, j| !j.start_with? "$"}.map {|_| _.size > 1 ? _ : _.last }
   
+  directory = {root: {size: 0, sub_folder: []}}
+  current_directory = :root
+  previous_directory = []
+
   res.each { |a|
     _ = parse a
     action, content = _[:Action], _[:content]
@@ -68,18 +84,39 @@ File.open('day07-input.txt', 'r') { |f|
     case action
     when "cd"
       if content
+        previous_directory << current_directory
+        current_directory = "#{current_directory}/#{content}"
+        directory[current_directory] = {size: 0, sub_folder: []}
       else
+        size = directory[current_directory][:size]
+        current_directory = previous_directory.pop
+        directory[current_directory][:size] += size
       end
     when "ls"
       content.each { |line|
-        /dir (?<folder_name>\w+)/ =~ line
+        /dir (?<folder_name>.+)/ =~ line
         /(?<size>\d+) (?<file_name>.+)/ =~ line
-        tree << { name: folder_name } if folder_name
-        tree << { name: file_name, size: } if size
+
+        directory[current_directory][:sub_folder] << folder_name if folder_name
+        directory[current_directory][:size] += size.to_i if size
       }
     else
       raise 'error'
     end
   }
-  puts tree
+
+  while !previous_directory.empty?
+    size = directory[current_directory][:size]
+    current_directory = previous_directory.pop
+    directory[current_directory][:size] += size
+  end
+
+  puts directory.reject { |key, value| value[:size] > 100000 }.sum { |key, value| value[:size] }
+
+  # p directory.each { |k, v| p [k, v[:size]]}
+
+  # dir = directory#.reject { |key, value| value[:size] > 100000 }#.sum { |key, value| value[:size] }
+
+  # dir.each {|e| puts e}
+
 }
