f9c6d507e77c8c2e89e62e7c91a64cff4921323c
noracami <kerke.2011@gmail.com>
2022-12-28 02:37:52 +0800

    day12 part1

--- day12-1.rb
+++ day12-1.rb
@@ -1,5 +1,5 @@
 class LinkedList
-  attr_accessor :neighbor, :steps_to_start
+  attr_accessor :neighbor, :reach, :previous, :steps_to_start
   attr_reader :name, :height
   
   @@S = nil
@@ -16,6 +16,8 @@ class LinkedList
   def initialize(height:, x:, y:)
     @name = "#{x}-#{y}"
     @neighbor = []
+    @reach = []
+    @previous = nil
     @steps_to_start = nil
 
     case height
@@ -24,7 +26,7 @@ class LinkedList
       @steps_to_start = 0
       @@S = self
     when 'E'
-      @height = 26
+      @height = 25
       @@E = self
     else
       @height = height.ord - 'a'.ord
@@ -32,12 +34,17 @@ class LinkedList
   end
   
   def calculate_steps
+    # if self.name == "18-4"
+    #   puts self
+    #   puts ".\n" * 5
+    #   sleep 100
+    # end
     if @steps_to_start.nil?
-      @steps_to_start = @neighbor
-        .map(&:steps_to_start)
-        .compact
-        .map(&:to_i).min + 1
+      # puts @reach
+      @previous = @reach.reject{|node| node.steps_to_start.nil? }.min_by{|node| node.steps_to_start.to_i}
+      @steps_to_start = @previous.steps_to_start + 1
     end
+    # puts self
   end
 
   def establish_link(node)
@@ -45,33 +52,31 @@ class LinkedList
 
     raise :node_must_be_a_LinkedList if node.class != LinkedList
     
-    if (node.height - self.height).abs <= 1
-      @neighbor << node
+    if node.height - self.height >= -1
       node.neighbor << self
+      self.reach << node
     end
+
+    if self.height - node.height >= -1
+      self.neighbor << node
+      node.reach << self
+    end
+
+    node.reach.uniq!
+    self.reach.uniq!
   end
 
   def to_s
     [
       "position: #{@name}",
       "height: #{@height}",
-      "neighbor: #{@neighbor.map(&:name)}",
+      # "neighbor: #{@neighbor.map(&:name)}",
+      # "reach: #{@reach.map(&:name)}",
+      "previous: #{@previous&.name}",
       "steps_to_start: #{@steps_to_start}"
     ].join("\t")
   end
 end
-
-# a = LinkedList.new(height: 'S', x: 0, y: 1)
-# b = LinkedList.new(height: 'b', x: 1, y: 1)
-# c = LinkedList.new(height: 'E', x: 2, y: 1)
-# a.establish_link(b)
-# a.establish_link(c)
-# b.establish_link(a)
-
-# puts a
-# puts a.neighbor
-
-# return
 
 INPUT = "day12-input.txt"
 SAMPLE = "day12-sample.txt"
@@ -82,39 +87,33 @@ File.open(INPUT, 'r') { |f|
       LinkedList.new(height: grid, x:, y:)
     }
   }
-  # X_SIZE = res.size
-  # Y_SIZE = res[0].size
   res.each_with_index { |line, x|
     line.each_with_index { |node, y|
       node.establish_link res[x-1][y] if x > 0
       node.establish_link res[x][y-1] if y > 0
     }
   }
-  # puts res[0][0]
-  # puts LinkedList.start_position
-  # puts LinkedList.best_signal_location
 
   current_nodes = [LinkedList.start_position]
-  # current_nodes = []
   
   while !current_nodes.empty?
     # puts current_nodes
     next_nodes = []
-    current_nodes.each { |curr_node|
-      # p 1
-      # sleep 1
-      next_nodes.concat(curr_node.neighbor.reject(&:steps_to_start))
-      # puts next_nodes
-      curr_node.calculate_steps
-      # p 5
-      # puts curr_node
-      # sleep 0.03
+    current_nodes.each { |current_node|
+      next_nodes.concat(current_node.neighbor.reject(&:steps_to_start))
+      current_node.calculate_steps
     }
-    current_nodes = next_nodes.uniq
-    # p current_nodes[0].class
+    next_nodes.uniq!
+    current_nodes = next_nodes
   end
+
   puts LinkedList.start_position
   p '---'
   puts LinkedList.best_signal_location
-  p res[20].map(&:steps_to_start).join('-')
+  p '---'
+  # current_nodes = LinkedList.best_signal_location
+  # while current_nodes.previous
+  #   current_nodes = current_nodes.previous
+  #   puts current_nodes
+  # end
 }
