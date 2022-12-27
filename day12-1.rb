883601fa36c5a67aa2f7ccbf85d846b57f118334
noracami <kerke.2011@gmail.com>
2022-12-27 22:00:38 +0800

    day12 wip

--- day12-1.rb
+++ day12-1.rb
@@ -1,5 +1,5 @@
 class LinkedList
-  attr_accessor :neighbor
+  attr_accessor :neighbor, :steps_to_start
   attr_reader :name, :height
   
   @@S = nil
@@ -21,6 +21,7 @@ class LinkedList
     case height
     when 'S'
       @height = 0
+      @steps_to_start = 0
       @@S = self
     when 'E'
       @height = 26
@@ -29,17 +30,13 @@ class LinkedList
       @height = height.ord - 'a'.ord
     end
   end
-
+  
   def calculate_steps
-    return @steps_to_start if @steps_to_start
-
-    steps = 1
-    neighbor = @neighbor
-    if neighbor.include? LinkedList.start_position
-      return steps
-    else
-      #TODO
-      neighbor = 
+    if @steps_to_start.nil?
+      @steps_to_start = @neighbor
+        .map(&:steps_to_start)
+        .compact
+        .map(&:to_i).min + 1
     end
   end
 
@@ -55,7 +52,12 @@ class LinkedList
   end
 
   def to_s
-    "position: #{@name}\theight: #{@height} neighbor: #{@neighbor.map(&:name)}"
+    [
+      "position: #{@name}",
+      "height: #{@height}",
+      "neighbor: #{@neighbor.map(&:name)}",
+      "steps_to_start: #{@steps_to_start}"
+    ].join("\t")
   end
 end
 
@@ -74,21 +76,45 @@ end
 INPUT = "day12-input.txt"
 SAMPLE = "day12-sample.txt"
 
-File.open(SAMPLE, 'r') { |f|
+File.open(INPUT, 'r') { |f|
   res = f.map.with_index { |line, x|
     line.chomp.chars.map.with_index { |grid, y|
       LinkedList.new(height: grid, x:, y:)
     }
   }
-  X_SIZE = res.size
-  Y_SIZE = res[0].size
+  # X_SIZE = res.size
+  # Y_SIZE = res[0].size
   res.each_with_index { |line, x|
     line.each_with_index { |node, y|
       node.establish_link res[x-1][y] if x > 0
       node.establish_link res[x][y-1] if y > 0
     }
   }
-  puts res[0][0]
+  # puts res[0][0]
+  # puts LinkedList.start_position
+  # puts LinkedList.best_signal_location
+
+  current_nodes = [LinkedList.start_position]
+  # current_nodes = []
+  
+  while !current_nodes.empty?
+    # puts current_nodes
+    next_nodes = []
+    current_nodes.each { |curr_node|
+      # p 1
+      # sleep 1
+      next_nodes.concat(curr_node.neighbor.reject(&:steps_to_start))
+      # puts next_nodes
+      curr_node.calculate_steps
+      # p 5
+      # puts curr_node
+      # sleep 0.03
+    }
+    current_nodes = next_nodes.uniq
+    # p current_nodes[0].class
+  end
   puts LinkedList.start_position
+  p '---'
   puts LinkedList.best_signal_location
+  p res[20].map(&:steps_to_start).join('-')
 }
