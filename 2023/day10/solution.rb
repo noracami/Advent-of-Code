ff886702adb1f861e29b7d168b90e6602a9fd9bf
noracami <kerke.2011@gmail.com>
2023-12-11 03:11:33 +0800

    ✅ day 10 part2

--- 2023/day10/solution.rb
+++ 2023/day10/solution.rb
@@ -9,7 +9,7 @@ class Solution
   DAY = 'day10'
   READ_SAMPLE = false
 
-  def initialize(_params)
+  def initialize(*)
     @components = { '|': [[0, 1], [0, -1]], '-': [[1, 0], [-1, 0]],
                     L: [[1, 0], [0, -1]], J: [[-1, 0], [0, -1]],
                     '7': [[-1, 0], [0, 1]], F: [[1, 0], [0, 1]] }
@@ -23,14 +23,16 @@ end
 
 def solution_one(input_data)
   build_maze(input_data)
-    .then do |maze|
-    maze[@start] = find_destination(@start[0], @start[1], components[:'7'])
-    bfs(maze, @start).size / 2
-  end
+    .then { |maze| bfs(maze, @start) }
+    .size / 2
 end
 
 def solution_two(input_data)
-  parse_backward(input_data).sum(&:last)
+  build_maze(input_data)
+    .then { |maze| bfs(maze, @start) }
+    .then { |visited| find_captured_points(input_data, visited) }
+    .flatten
+    .count(:captured_animals)
 end
 
 #
@@ -60,8 +62,38 @@ def build_maze(input_data)
     .flatten(1)
     .compact
     .each_with_object({}) { |(x, y, char), memo| memo[[x, y]] = find_destination(x, y, char) }
+    .update({ @start => find_destination(@start[0], @start[1], components[:'7']) })
 end
 
+def find_captured_points(input_data, visited)
+  input_data.map.with_index(1) do |line, y|
+    status = { encountered_pipe: nil, can_capture: false }
+    line.chars.map.with_index(1) do |char, x|
+      if visited[[x, y]].zero?
+        :captured_animals if status[:can_capture]
+      else
+        handle_encountered_pipe(status, char)
+      end
+    end
+  end
+end
+
+def handle_encountered_pipe(status, char, previous_char = status[:encountered_pipe])
+  if (char == '|') || (char == 'J' && (previous_char == 'F')) || (char == '7' && (previous_char == 'L'))
+    status[:can_capture] = !status[:can_capture]
+  end
+
+  update_encountered_pipe(status, char)
+
+  nil
+end
+
+def update_encountered_pipe(status, char)
+  status[:encountered_pipe] = char if %w[F L].include?(char)
+  status[:encountered_pipe] = nil if %w[J 7].include?(char)
+end
+
+# rubocop:disable Metrics/MethodLength
 def bfs(maze, start)
   queue = [start]
   visited = Hash.new(0)
@@ -82,3 +114,4 @@ def bfs(maze, start)
   end
   visited
 end
+# rubocop:enable Metrics/MethodLength
