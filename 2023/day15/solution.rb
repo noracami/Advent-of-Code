fef3a1a1781e1593d3311d04f2065c4ee9c258a5
noracami <kerke.2011@gmail.com>
2023-12-16 01:58:10 +0800

    ♻️ refactor day 15

--- 2023/day15/solution.rb
+++ 2023/day15/solution.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require 'English'
-
 class Solution
   include AdventOfCodeFileIO
   attr_reader :day, :answer, :scope
@@ -15,63 +13,51 @@ end
 ### build solution
 
 def solution_one(input_data)
-  # @debug = true
-  # @res = 0
-  hash_it = ->(input) {
-    input.codepoints.reduce(0) do |memo, ascii_code|
-      (memo + ascii_code).then { multiply(_1, 17) }.then { get_remainder(_1, 256) }
-    end
-  }
-
-  input_data[0].split(',').map(&hash_it).sum
+  parse(input_data).sum { hash_it _1 }
 end
 
 def solution_two(input_data)
-  hash_it = ->(input) {
-    input.codepoints.reduce(0) do |memo, ascii_code|
-      (memo + ascii_code).then { multiply(_1, 17) }.then { get_remainder(_1, 256) }
-    end
-  }
-
   regex = /([a-z]+)([=-])(\d?)/
-  boxes = 256.times.map { |i| [i, { hash: {}, list: []}] }.to_h
+  boxes = 256.times.to_h { |i| [i, { hash: {}, list: [] }] }
 
-  input_data[0]
-    .split(',')
+  parse(input_data)
     .each do |step|
-      step.scan(regex) do |m|
-        label, sign, value = m
-        box_no = hash_it.(label)
-        case sign
-        when '='
-          if boxes[box_no][:hash][label]
-            boxes[box_no][:hash][label][:value] = value
-          else
-            boxes[box_no][:hash][label] = { value: value, label: label }
-            boxes[box_no][:list] << boxes[box_no][:hash][label]
-          end
-        when '-'
-          if boxes[box_no][:hash][label]
-            boxes[box_no][:list].delete_if { |h| h[:label] == label }
-            boxes[box_no][:hash].delete(label)
-          end
+      label, sign, value = step.scan(regex)[0]
+      box_no = hash_it(label)
+      case sign
+      when '='
+        if boxes[box_no][:hash][label]
+          boxes[box_no][:hash][label][:value] = value
+        else
+          boxes[box_no][:hash][label] = { value:, label: }
+          boxes[box_no][:list] << boxes[box_no][:hash][label]
+        end
+
+      when '-'
+        if boxes[box_no][:hash][label]
+          boxes[box_no][:list].delete_if { |h| h[:label] == label }
+          boxes[box_no][:hash].delete(label)
         end
       end
     end
-    boxes
-      .reject { |_, v| v[:hash].empty? }
-      .map { |box_no, v| v[:list].map.with_index(1) { |h, index| index * h[:value].to_i }.sum * (box_no + 1) }
-      .sum
+
+  boxes
+    .reject { |_, v| v[:hash].empty? }
+    .sum { |box_no, v| v[:list].map.with_index(1) { |h, index| index * h[:value].to_i }.sum * (box_no + 1) }
 end
 
 #
 #
 ### build methods
 
-def multiply(input, factor)
-  input * factor
+def parse(input_data)
+  input_data[0].split(',')
 end
 
-def get_remainder(input, divisor)
-  input % divisor
+def hash_it(input)
+  input.codepoints.reduce(0) do |memo, ascii_code|
+    (memo + ascii_code)
+      .then { |value| value * 17 }
+      .then { |value| value % 256 }
+  end
 end
