d75cf8c0162d4731a12a96cc329191050d3eb343
noracami <kerke.2011@gmail.com>
2023-12-18 03:22:15 +0800

    🚧

--- 2023/day17/solution.rb
+++ 2023/day17/solution.rb
@@ -43,10 +43,14 @@ def build_boundary(data)
   @boundary_col = data[0].size - 1
 end
 
-def get_neighbors(row = 0, col = 0, cost = 0, previous_direction = [])
+def get_neighbors(node)
+  row = node[:row]
+  col = node[:col]
+  cost = node[:cost]
+  previous_directions = node[:directions]
   neighbors = []
-  not_allowed_direction = if previous_direction.chunk_while { |a, b| a == b }.map(&:size).last.to_i == 2
-                            previous_direction.last
+  not_allowed_direction = if previous_directions.chunk_while { |a, b| a == b }.map(&:size).last.to_i == 2
+                            previous_directions.last
                           end
 
   [[row - 1, col], [row + 1, col], [row, col - 1], [row, col + 1]]
@@ -58,57 +62,84 @@ def get_neighbors(row = 0, col = 0, cost = 0, previous_direction = [])
     # skip if out of bounds
     next if r.negative? || c.negative? || r > boundary_row || c > boundary_col
 
-    neighbors << { row: r, col: c, cost: cost + heat_lose_map[r][c].to_i, direction: }
+    neighbors << {
+      row: r,
+      col: c,
+      cost: cost + heat_lose_map[r][c].to_i,
+      directions: previous_directions + [direction]
+    }
   end
 
   neighbors
 end
 
 def dijkstra(start_node = [0, 0], end_node = [boundary_row, boundary_col], max_same_directions: 3)
-  queue = [start_node, 0]
+  queue = [{ row: start_node[0], col: start_node[1], cost: 0, directions: [] }]
   visited = {}
   costs = {}
   parents = {}
 
-  costs[start_node] = 0
+  counter = 0
 
   until queue.empty?
-    queue.sort_by! { |_, heat, _| heat }
+    queue.sort_by! { |node| node[:cost] }
+    # pp queue
     current_node = queue.shift
+    # puts current_node
+    # counter += 1
+    # puts "counter: #{counter}"
+    # read = $stdin.gets.chomp
+    # return :quit if read == 'q'
 
     # skip if visited
-    next if visited[current_node]
+    if visited[[current_node[:row], current_node[:col]]]
+      # pp _ = {
+      #   current_node_cost: current_node[:cost],
+      #   cmp_costs: costs[[current_node[:row], current_node[:col]]]
+      # }
+      # puts "counter: #{counter}"
+      # read = $stdin.gets.chomp
+      # return :quit if read == 'q'
+
+      next
+    end
 
     # mark as visited
-    visited[current_node] = true
+    visited[[current_node[:row], current_node[:col]]] = true
+    costs[[current_node[:row], current_node[:col]]] = current_node[:cost]
 
     # break if end node
-    break if current_node == end_node
+    if end_node == [current_node[:row], current_node[:col]]
+      pp current_node
+      break
+    end
 
     # get neighbors
-    neighbors = get_neighbors(*current_node, heat, parents[current_node] || [])
+    neighbors = get_neighbors(current_node)
 
     # add neighbors to queue
     neighbors.each do |neighbor|
       # skip if visited
-      next if visited[neighbor]
-
-      # # skip if cost is greater than max_same_directions
-      # next if costs[current_node] >= max_same_directions
+      next if visited[[neighbor[:row], neighbor[:col]]]
 
       # add neighbor to queue
       queue << neighbor
 
       # set cost
-      costs[neighbor] = costs[current_node] + neighbor[:cost]
+      # pp costs
+      # pp neighbor
+      # pp costs[current_node]
+      # pp current_node
+      # costs[neighbor] = costs[[current_node[:row], current_node[:col]]] + neighbor[:cost]
 
-      # set parent
-      parents[neighbor] = current_node
+      # # set parent
+      # parents[neighbor] = current_node
     end
   end
 
   # return costs
-  costs
+  # pp counter
+  costs.max
 end
 
 def puts_debug_message(key = nil, value:)
