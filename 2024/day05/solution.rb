dfee179b66eb5705cca6bd0c9f160cbc26761dc6
ker ker <kerke.2011@gmail.com>
2024-12-05 15:28:35 +0800

    📝 2024/05-2

--- 2024/day05/solution.rb
+++ 2024/day05/solution.rb
@@ -5,9 +5,9 @@ def solution_one(filename, input = nil)
   puts 'using input' if input
   puts filename
   data = input || File.readlines(filename, chomp: true)
-  right_section, second_section = data.slice_when { _1.empty? }.to_a
-  right_section.reject!(&:empty?)
-  right_section = right_section.each_with_object({}) do |line, hash|
+  first_section, second_section = data.slice_when { _1.empty? }.to_a
+  first_section.reject!(&:empty?)
+  first_section = first_section.each_with_object({}) do |line, hash|
     key, value = line.split('|')
     if hash[key]
       hash[key] << value
@@ -15,7 +15,7 @@ def solution_one(filename, input = nil)
       hash[key] = [value]
     end
   end
-  # puts right_section
+  # puts first_section
 
   ret = 0
   second_section.each do |line|
@@ -24,9 +24,9 @@ def solution_one(filename, input = nil)
     flag_cnt = true
     while arr.size > 1 && flag_cnt
       key = arr.shift
-      value = right_section[key]
+      value = first_section[key]
       if value.nil?
-        # puts "#{key} is not found in the right section"
+        # puts "#{key} is not found in the first section"
         # puts "break"
         flag_cnt = false
         break
@@ -44,14 +44,14 @@ def solution_one(filename, input = nil)
       end
 
       # if flag_cnt
-      #   puts "#{key} is place in the right section"
+      #   puts "#{key} is place in the first section"
       # else
-      #   puts "#{key} is not place in the right section"
+      #   puts "#{key} is not place in the first section"
       # end
     end
 
     if flag_cnt
-      # means all the keys are placed in the right section
+      # means all the keys are placed in the first section
       # add the value to the result
       targets = line.split(',')
       target = targets[targets.size / 2]
@@ -63,108 +63,96 @@ def solution_one(filename, input = nil)
 
     # puts "end of line"
   end
-  # puts second_section
-  # ret = ''
-  # two_d_map = data.map { _1.chars }
-  # # find the target in 8 directions
-  # # given = (x, y)
-  # # directions = [
-  # #   [-1,  1], [0,  1], [1,  1],
-  # #   [-1,  0],          [1,  0],
-  # #   [-1, -1], [0, -1], [1, -1]
-  # # ]
-  # @global_2d_map = two_d_map
-  # @canvas = two_d_map.map { |row| row.map { '.' } }
-  # @memo = []
-  # def is_valid(origin_x, origin_y, directions, unit)
-  #   cells = []
-  #   unit.times do |i|
-  #     row = @global_2d_map[origin_y + directions[1] * i]
-  #     return false if row.nil? || origin_y + directions[1] * i < 0
-  #     col = row[origin_x + directions[0] * i]
-  #     return false if col.nil? || origin_x + directions[0] * i < 0
-
-  #     cells << col
-  #   end
-  #   result = cells.join == 'XMAS'
-  #   if result
-  #     @memo << [origin_x, origin_y, directions, unit]
-  #     unit.times do |i|
-  #       @canvas[origin_y + directions[1] * i][origin_x + directions[0] * i] = 'XMAS'[i]
-  #     end
-  #   end
-
-  #   result
-  # end
-
-  # ret = 0
-
-  # eight_directions = [ [-1,  1], [0,  1], [1,  1], [-1,  0], [1,  0], [-1, -1], [0, -1], [1, -1] ]
-
-  # two_d_map.each_with_index do |row, y|
-  #   row.each_with_index do |cell, x|
-  #     eight_directions.each do |direction|
-  #       ret += 1 if is_valid(x, y, direction, 4)
-  #     end
-  #   end
-  # end
 
   puts "answer: #{ret}"
-  # puts @canvas.map(&:join)
-  # puts @canvas.flatten.count('.')
-  # puts @memo.map { |x, y, d, u| "#{x}, #{y}, #{d}, #{u}" }
 end
 
 solution_one(sample_file)
 solution_one(puzzle_file)
 
-# def solution_two(filename)
-#   puts filename
-#   data = File.readlines(filename, chomp: true)
+def solution_two(filename)
+  puts filename
+  data = File.readlines(filename, chomp: true)
+  first_section, second_section = data.slice_when { _1.empty? }.to_a
+  first_section.reject!(&:empty?)
+  first_section = first_section.each_with_object({}) do |line, hash|
+    key, value = line.split('|')
+    if hash[key]
+      hash[key] << value
+    else
+      hash[key] = [value]
+    end
+  end
+  # puts first_section
 
-#   @global_2d_map = data.map(&:chars)
-#   @canvas = @global_2d_map.map { |row| row.map { '.' } }
+  ret = 0
+  # second_section[0, 6].each do |line|
+  second_section.each do |line|
+    arr = line.split(',')
+    # puts "===\ncurrent line: #{line}\n"
+    flag_cnt = true
+    while arr.size > 1 && flag_cnt
+      key = arr.shift
+      value = first_section[key]
+      if value.nil?
+        # puts "#{key} is not found in the first section"
+        # puts "break"
+        flag_cnt = false
+        break
+      end
 
-#   def is_valid(origin_x, origin_y)
-#     if origin_x < 1 || origin_y < 1 || origin_x > @global_2d_map[0].size - 2 || origin_y > @global_2d_map.size - 2
-#       # puts "out of bound: #{origin_x}, #{origin_y}"
-#       return false
-#     end
+      arr.each do |v|
+        if value.include?(v)
+          next
+        else
+          # puts "#{v} is not found in the #{key}: #{value}"
+          # puts "break"
+          flag_cnt = false
+          break
+        end
+      end
 
-#     lines = []
-#     lines << @global_2d_map[origin_y - 1][origin_x - 1] + @global_2d_map[origin_y + 1][origin_x + 1]
-#     lines << @global_2d_map[origin_y - 1][origin_x + 1] + @global_2d_map[origin_y + 1][origin_x - 1]
+      # if flag_cnt
+      #   puts "#{key} is place in the first section"
+      # else
+      #   puts "#{key} is not place in the first section"
+      # end
+    end
 
-#     result = lines.all? { |line| line.count('M') == 1 && line.count('S') == 1 }
-#     # if origin_x == 2 && origin_y == 1
-#     #   puts "lines: #{lines}"
-#     #   puts "result: #{result}, #{origin_x}, #{origin_y}"
-#     # end
+    if flag_cnt
+      # means all the keys are placed in the first section
+      # discard then in solution two
+    else
+      # to correctly the order of the keys, by first_section
+      targets = line.split(',')
+      counting_arr = targets.map { |target| [target, 0] }.to_h
+      targets.each do |target|
+        first_section.each do |key, value|
+          if targets.include?(key) && value.include?(target)
+            counting_arr[target] += 1
+          end
+        end
+      end
+      targets.sort_by! { |target| counting_arr[target] }
 
-#     # if result
-#     #   @canvas[origin_y - 1][origin_x - 1] = @global_2d_map[origin_y - 1][origin_x - 1]
-#     #   @canvas[origin_y + 1][origin_x - 1] = @global_2d_map[origin_y + 1][origin_x - 1]
-#     #   @canvas[origin_y - 1][origin_x + 1] = @global_2d_map[origin_y - 1][origin_x + 1]
-#     #   @canvas[origin_y + 1][origin_x + 1] = @global_2d_map[origin_y + 1][origin_x + 1]
-#     #   @canvas[origin_y][origin_x] = @global_2d_map[origin_y][origin_x]
-#     # end
+      # puts "line:   #{line}"
+      # puts "sorted: #{targets.join(',')}"
+      # puts "counting: #{counting_arr.sort_by { |k, v| -v }.to_h}"
 
-#     result
-#   end
 
-#   ret = 0
+      # puts "line:   #{line}"
+      # puts "sorted: #{targets.join(',')}"
 
-#   @global_2d_map.each_with_index do |row, y|
-#     row.each_with_index do |cell, x|
-#       ret += 1 if cell == 'A' && is_valid(x, y)
-#     end
-#   end
+      ret += targets.reverse[targets.size / 2].to_i
+    end
 
-#   puts "answer: #{ret}"
-#   # puts @canvas.map(&:join)
-# end
+    # puts "end of line"
+  end
 
-# # sample2_file = File.join(File.dirname(__FILE__), 'sample2.txt')
+  # puts "first section: #{first_section.values.map(&:size).tally}"
 
-# solution_two(sample_file)
-# solution_two(puzzle_file)
+  puts "answer: #{ret}"
+end
+
+solution_two(sample_file)
+solution_two(puzzle_file)
