c5bffd7f6ddfb062a9cd389cc5335eb26d7676cf
noracami <kerke.2011@gmail.com>
2025-12-08 18:12:38 +0800

    🎉 2025 Q8 part 2

--- 2025/day08/solution.rb
+++ 2025/day08/solution.rb
@@ -88,44 +88,82 @@ end
 solution_one(sample_file)
 solution_one(puzzle_file, 1000)
 
+# Disjoint Set (Union-Find)
+class DisjointSet
+  def initialize(points)
+    @parent = {}
+    points.each { |p| @parent[p] = p }
+  end
+
+  def find(point)
+    if @parent[point] == point
+      point
+    else
+      @parent[point] = find(@parent[point])
+      @parent[point]
+    end
+  end
+
+  def union(point_a, point_b)
+    root_a = find(point_a)
+    root_b = find(point_b)
+
+    unless root_a == root_b
+      @parent[root_a] = root_b
+      true
+    else
+      false
+    end
+  end
+end
+
+# Kruskal find MST
+def find_minimum_spanning_tree(points)
+  edges = []
+
+  edges = points.combination(2)
+                .map { |p1, p2| Distance.between(p1, p2) }
+                .sort_by(&:value)
+
+  dsu = DisjointSet.new(points)
+  minimum_spanning_tree_edges = []
+  total_distance = 0
+
+  edges.each do |edge|
+    p1 = edge.point1
+    p2 = edge.point2
+    distance = edge.value
+
+    if dsu.union(p1, p2)
+      minimum_spanning_tree_edges << edge
+      total_distance += distance
+
+      break if minimum_spanning_tree_edges.length == points.length - 1
+    end
+  end
+
+  {
+    total_distance: total_distance,
+    edges: minimum_spanning_tree_edges
+  }
+end
+
 def solution_two(filename)
   puts 'run part 2'
   puts filename
   data = File.readlines(filename, chomp: true)
 
-  # add boundary
-  arr = data.map(&:chars).map { |line| ['b'] + line + ['b'] }
-  arr.push(arr[0].size.times.map { 'b' })
-
-  arr.each.with_index do |line, row|
-    line.each.with_index do |_val, col|
-      process!([row, col], arr)
-    end
+  points = data.map do |line|
+    x, y, z = line.split(',').map(&:to_i)
+    Point.new x:, y:, z:
   end
 
-  ret = 0
+  tree = find_minimum_spanning_tree(points)
 
-  arr.reverse!
-  arr.each.with_index do |line, row|
-    line.each_index do |col|
-      case arr[row][col]
-      when 'b', '.'
-        next
-      when '|'
-        arr[row][col] = arr[row - 1][col].to_i.nonzero? || 1
-      when '^'
-        arr[row][col] = arr[row - 1][col - 1] + arr[row - 1][col + 1]
-      when 'S'
-        ret = arr[row - 1][col]
-        break
-      end
-    end
-  end
-
-  # arr.each { |l| puts l.join("\t") }
+  ret = tree[:edges].last.then { |edge| edge.point1.x * edge.point2.x }
 
   puts "answer: #{ret}"
 end
 
-# solution_two(sample_file)
-# solution_two(puzzle_file)
+solution_two(sample_file)
+solution_two(puzzle_file)
